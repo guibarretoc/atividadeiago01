70ec06366dce67e6267f73ca3104a110b59c3e8f
guibarretoc <118860169+guibarretoc@users.noreply.github.com>
2023-08-17 21:32:00 -0300

    Add files via upload

---
+++ Atividade01/src/Main/Agencia.java
@@ -0,0 +1,5 @@
+package Main;
+
+public class Agencia {
+	int numeroAgencia;
+}
---
+++ Atividade01/src/Main/Cliente.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class Cliente {
+	int conta;
+	String cpf;
+	String nome;
+	String email;
+	int dataDeNascimento;
+	int telefone;
+	String enderco;
+	
+	
+}
